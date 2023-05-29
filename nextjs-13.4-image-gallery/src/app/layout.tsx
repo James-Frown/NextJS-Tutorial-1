5aafde3a8872c35821738d7be7eeb7022126cf1f
James-Frown <21122@ctca.co.za>
2023-05-29 17:02:13 +0200

    Newest Changes | Added Readme & Content Changes

--- nextjs-13.4-image-gallery/src/app/layout.tsx
+++ nextjs-13.4-image-gallery/src/app/layout.tsx
@@ -1,10 +1,13 @@
+// this is the global css & applied to all pages
 import './globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// defines the metadata of a webpage
 export const metadata = {
   // This is the global Title & the fallback
+  // can set different titles in sub pages later
   title: "NextJS Image Gallery",
   // This is the global description
   description: "Tutorial Project by Coding In Flow",
@@ -16,6 +19,9 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    // This layout wraps the whole application in the home page tsx file
+    // it is rendered as the childeren in the body tag
+    // the layout is where you can put the navbar
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
