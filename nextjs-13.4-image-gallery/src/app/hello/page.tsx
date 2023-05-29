334a756aa9063f755a74f8ec180d8e98497a0548
James-Frown <21122@ctca.co.za>
2023-05-29 17:55:11 +0200

    Newest Changes | Adde Content Changes

--- nextjs-13.4-image-gallery/src/app/hello/page.tsx
+++ nextjs-13.4-image-gallery/src/app/hello/page.tsx
@@ -1,7 +1,7 @@
 // main example - server side rendered
 export default async function Page() {
     await new Promise((resolve) => setTimeout(resolve, 1000));
-    throw Error("Bazninga");
+    // throw Error("Bazninga");
     return <><div><h1>Hello, NextJS 13.5</h1></div></>
 }
 
