fd6aeb6c2d8812f32033fc930f16b409bdf6ceb1
James-Frown <21122@ctca.co.za>
2023-05-30 22:07:17 +0200

    Newest Changes | added the unsplash api which returns the rendered image

--- nextjs-13.4-image-gallery/src/app/static/page.tsx
+++ nextjs-13.4-image-gallery/src/app/static/page.tsx
@@ -1,8 +1,9 @@
 //import the unsplash api key
 import { UnsplashImage } from "@/models/unsplash-image";
 import Image from "next/image";
+import Link from "next/link";
 
-// function to fetch the unsplashed api
+// function to fetch the unsplashed api data as json & return an image
 export default async function Page() {
     const response = await fetch("https://api.unsplash.com/photos/random?client_id=" + process.env.UNSPLASH_ACCESS_KEY);
     const image: UnsplashImage = await response.json();
@@ -19,7 +20,9 @@ export default async function Page() {
                     width={width}
                     height={height}
                     alt={image.description}
+                    className="rounded shadow mw-100 h-100"
                 />
+                by <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
             </div>
         </>
     );
