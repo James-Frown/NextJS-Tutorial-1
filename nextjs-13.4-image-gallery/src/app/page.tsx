046254739f445ddb290cdc15ebc728a2b483c70d
James-Frown <21122@ctca.co.za>
2023-05-30 10:37:11 +0200

    Newest Changes | Added Changes to home page & removed the css file for its styling

--- nextjs-13.4-image-gallery/src/app/page.tsx
+++ nextjs-13.4-image-gallery/src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-// this is the CSS file & it is directed spesifically to this page
-import styles from './page.module.css'
-
 export default function Home() {
   return (
     <>
