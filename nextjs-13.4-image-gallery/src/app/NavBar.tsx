bdd3dbe098703704a2d3476cbd50aa5bcdc41079
James-Frown <21122@ctca.co.za>
2023-05-30 11:04:14 +0200

    Newest Changes | Added Changes to navbar to stop href refresh, instead using as tag and now there is no refreshing

--- nextjs-13.4-image-gallery/src/app/NavBar.tsx
+++ nextjs-13.4-image-gallery/src/app/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 export default function NavBar() {
@@ -8,17 +9,19 @@ export default function NavBar() {
     // sticky sets its alighnment
     // expand sm makes it dynamic & resizeable, it ablso makes it have a hamburger menue
     // collapse on select is good for when you click a link
+    // ther eis an issue with state and cash when using href so try not to instead use:
+    // <element> as (which declares what it will act like) {link (wich dosnt refresh page / state)}
     return (
         <>
             <Navbar bg="primary" variant="dark" sticky="top" expand="sm" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
+                    <Navbar.Brand as={Link} href="/">
                         NextJS Image Gallery
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="main-navbar" />
                     <Navbar.Collapse id="main-navbar">
                         <Nav>
-                            <Nav.Link href="/hello">
+                            <Nav.Link as={Link} href="/hello">
                                 Hello
                             </Nav.Link>
                         </Nav>
