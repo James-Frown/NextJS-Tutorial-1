f1ffb31d84462230006427e0eb113eb943b74065
James-Frown <21122@ctca.co.za>
2023-06-01 21:55:05 +0200

    Newest Changes |

--- nextjs-13.4-image-gallery/src/app/NavBar.tsx
+++ nextjs-13.4-image-gallery/src/app/NavBar.tsx
@@ -51,6 +51,9 @@ export default function NavBar() {
                                     Coding
                                 </NavDropdown.Item>
                             </NavDropdown>
+                            <Nav.Link as={Link} href="/search" active={pathname === "/search"}>
+                                Search
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
