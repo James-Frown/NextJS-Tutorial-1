1e87efdecef5d7b45e3945bdd97f3f0ab6a34793
James-Frown <21122@ctca.co.za>
2023-06-01 21:00:02 +0200

    Newest Changes |

--- nextjs-13.4-image-gallery/src/app/(CSR)/search/SearchPage.tsx
+++ nextjs-13.4-image-gallery/src/app/(CSR)/search/SearchPage.tsx
@@ -1,11 +1,37 @@
 "use client"
 
+import { FormEvent } from "react";
+import { Form, Button } from "react-bootstrap";
+
 export default function SearchPage() {
-    return(
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const query = formData.get("query")?.toString().trim();
+
+        // when you need api keys use the process.env.(key) as it will keep your private api keys away from client browser and on your own backend server.
+        if (query) {
+            alert(query);
+        }
+    }
+
+    return (
         <>
-        <div>
-            Search
-        </div>
+            <div>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="search-input">
+                        <Form.Label>Search query</Form.Label>
+                        <Form.Control
+                            name="query"
+                            placeholder="E.g. Cats, Hotdogs, ..."
+                        />
+                    </Form.Group>
+                    <Button type="submit" className="rb-3">
+                        Search
+                    </Button>
+                </Form>
+            </div>
         </>
     );
 }
